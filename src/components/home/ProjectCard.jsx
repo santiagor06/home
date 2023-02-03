49ffb8d8292058ba67d8dfcd04f3d067550f5373
santiagor06 <santiago.restrepo06@hotmail.com>
2023-02-03 17:44:19 -0500

    add technologies

--- src/components/home/ProjectCard.jsx
+++ src/components/home/ProjectCard.jsx
@@ -5,53 +5,106 @@ import Row from 'react-bootstrap/Row';
 import url from "../../image/miniatura.png"
 import poke from "../../image/pokemon.png"
 import uplo from "../../image/upload.png"
+import issue from "../../image/gitIssue.png"
 import Button from "react-bootstrap/Button"
 import Container from 'react-bootstrap/Container';
+import Badge from 'react-bootstrap/Badge';
 
 
 const ProjectCard = ({ value }) => {
-
+const techURL=["Javascript","React","Tailwind","PostgreSQL","Node.js","Express","Firebase"]
+const techPoke=["Javascript","React","CSS","PostgreSQL","Node.js","Express"]
+const techUpload=["Javascript","React","Bootstrap","Cloudinary"]
   return (
    <Container>
-    <Row className="justify-content-md-center">
-    <Col xs={4}>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={url} />
+    <Row  className="g-4 justy-content align-item" xs={2} md={4} >
+    <Col >
+    <Card style={{ width: '18rem' ,height: '400px' }}>
+      <Card.Img style={{ height: '150px' }} variant="top" src={url} />
       <Card.Body>
         <Card.Title>urLearning</Card.Title>
         <Card.Text>
-        A website for the sale of online courses.the technologies used were postgreSQL,node.js and express for the back,for the front we used react,redux and tailwind,we also used firebase for the user authentication. 
+       A web application for the sale of online courses, with shopping cart and user authentication.
+        <div>
+          {techURL.map(tech=>{
+          return (<Badge pill bg="primary">
+        {tech}
+      </Badge>)}
+            )}
+ 
+          </div>
         </Card.Text>
         <a href="https://pf-ur-learning.vercel.app/"><Button variant="light">View 🚀</Button></a>
       </Card.Body>
     </Card>
     </Col>
-    <Col xs={4}>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={poke} />
+    <Col >
+    <Card style={{ width: '18rem' ,height: '400px' }}>
+      <Card.Img style={{ height: '150px' }} variant="top" src={poke} />
       <Card.Body>
         <Card.Title>PokeAPP</Card.Title>
         <Card.Text>
-        I designed and developed a pokemon App that included searching, filtering, sorting and creating pokemons.for the Front I used React, Redux and CSS.for the Back I used, Node.js with Express.
+        I designed and developed a pokemon App that included searching, filtering and sorting.  
+        <div>
+          {techPoke.map(tech=>{
+          return (<Badge pill bg="primary">
+        {tech}
+      </Badge>)}
+            )}
+ 
+          </div>
 
         </Card.Text>
         <a href="https://pokeapp-ochre.vercel.app/"><Button variant="light">View 🚀</Button></a>
       </Card.Body>
     </Card>
     </Col>
-    <Col xs={4}>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={uplo} />
+    <Col >
+    <Card style={{ width: '18rem' ,height: '400px' }}>
+      <Card.Img variant="top" src={issue} style={{ height: '150px' }}  />
+      <Card.Body>
+        <Card.Title>Git Issues</Card.Title>
+        <Card.Text>
+        An application that allows you to upload files and gives you a url to access it.
+        <div>
+          {techUpload.map(tech=>{
+          return (<Badge pill bg="primary">
+        {tech}
+      </Badge>)}
+            )}
+ 
+          </div></Card.Text>
+        
+        <a href="https://imageupload-omega.vercel.app/"><Button variant="light">View 🚀</Button></a>
+      </Card.Body>
+    </Card>
+    </Col>
+    <Col>
+    <Card style={{ width: '18rem' ,height: '400px' }}>
+      <Card.Img variant="top" src={uplo} style={{ height: '150px' }}  />
       <Card.Body>
         <Card.Title>Upload Image</Card.Title>
         <Card.Text>
-        An application that allows you to upload files and gives you a url to access it. I used React and bootstrap for the front end, cloudinary to upload the files.        </Card.Text>
+        An application that allows you to upload files and gives you a url to access it.
+        <div>
+          {techUpload.map(tech=>{
+          return (<Badge pill bg="primary">
+        {tech}
+      </Badge>)}
+            )}
+ 
+          </div></Card.Text>
+        
         <a href="https://imageupload-omega.vercel.app/"><Button variant="light">View 🚀</Button></a>
       </Card.Body>
     </Card>
     </Col>
 
+
     </Row>
+    
+
+    
    </Container>
      
    
